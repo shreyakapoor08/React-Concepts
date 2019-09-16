77fa12dd699ffd229201727edc0edc147bdc42c1
shreyakapoor08 <shreyakapoor98@gmail.com>
2019-09-16 11:07:19 +0530

    Person component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() { //react will call this method to render something to the screen
+      //<Person/> is a self closing tag
      return (
        <div className="App">
          <h1>Hi I'm a react app</h1>
-         <p>THis is working</p>
+         <p>This is working</p>
+          <Person/>
+          <Person/>
        </div> //this part of code is jsx which looks like html
        // <p>This does not wrk</p>  as jsx can have only 1 root element
      );
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,9 @@
+//usually function name start with lowercase
+import React from 'react';
+
+const person = () => {
+    //component is a function which is returning some jsx
+    return <p>I'm a Person</p>
+};
+
+export default person;
