0c2c4a773696d3c9699333233aa2775925b6cc25
shreyakapoor08 <shreyakapoor98@gmail.com>
2019-09-18 15:33:18 +0530

    react formm done

--- react-form/src/App.js
+++ react-form/src/App.js
@@ -1,55 +1,38 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router, Route, Link, NavLink} from 'react-router-dom';
+import SignUpForm from './pages/SignUpForm'
+import SignInForm from './pages/SignInForm'
 import './App.css';
 
 class App extends Component{
   render() {
     return(
-        <div className="App">
-         <div className="App__Aside">
+        <Router>
+          <div className="App">
+            <div className="App__Aside">
 
-         </div>
-          <div className="App__Form">
-            <div className="PageSwitcher">
-              <a href="#" className="PageSwitcher__Item">Sign In</a>
-              <a href="#" className="PageSwitcher__Item PageSwitcher__Item--Active">Sign Up</a>
             </div>
+            <div className="App__Form">
+              <div className="PageSwitcher">
+                <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+                <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+              </div>
 
-            <div className="FormTitle">
-              <a href="#" className="FormTitle__Link">Sign In</a> or <a href="#" className="FormTitle__Link FormTitle__Link--Active">Sign Up</a>
-            </div>
+              <div className="FormTitle">
+                <NavLink to='/sign-in' activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to='/' activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+              </div>
 
-            <div className="FormCenter">
-              <form className="FormFields" onSubmit={this.handleSubmit}>
-                <div className="FormField">
-                  <label htmlFor="name" className="FormField__Label">Full Name</label>
-                  <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name"/>
-                </div>
+              {/*wrapping the form which is supposed to be changed into a Route element*/}
+              <Route exact path="/" component={SignUpForm}>
 
-                <div className="FormField">
-                  <label className="FormField__Label" htmlFor="password">Password</label>
-                  <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" />
-                </div>
+              </Route>
+              <Route path="/sign-in" component={SignInForm}>
 
-                <div className="FormField">
-                  <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                  <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" />
-                </div>
-
-                <div className="FormField">
-                  <label className="FormField__CheckboxLabel">
-                    <input className="FormField__Checkbox" type="checkbox" name="hasAgreed" /> I agree all statements in <a href="" className="FormField__TermsLink">terms of service</a>
-                  </label>
-                </div>
-
-                <div className="FormField">
-                  <button className="FormField__Button mr-20">Sign Up</button>
-                  <a href="#" className="FormField__Link">I'm already member</a>
-                </div>
-              </form>
+              </Route>
             </div>
           </div>
-        </div>
+        </Router>
+
     )
   }
 }
