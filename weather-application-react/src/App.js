8f7768a4c5ed0b814dfa5029b7ef693640e39d67
shreyakapoor08 <shreyakapoor98@gmail.com>
2019-09-19 15:14:17 +0530

    weather app

--- weather-application-react/src/App.js
+++ weather-application-react/src/App.js
@@ -8,19 +8,73 @@ import Weather from "./components/Weather";
 const API_KEY = "70711dbdffa620ff91964370d759fc94";
 
 class App extends React.Component{
-    getWeather = async(e) => {
-        e.preventDefault();
+    state = {
+    //    state is a ob and containes key value pair
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: undefined
+    };
+    getWeather = async(event) => {
+        event.preventDefault();
+        const city = event.target.elements.city.value;
+        const country = event.target.elements.country.value;
+    //    preventDefault is used in single page application
     //    arrow function allow to use this keyword independently
-        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
         const data = await api_call.json();
-        console.log(data);
+        if(city && country)
+        {
+            console.log(data);
+            this.setState({
+                //here is the temperature live
+                temperature: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.main.humidity,
+                description: data.weather[0].description,
+                error: ""
+            });
+        } else {
+            this.setState({
+                //here is the temperature live
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error: "Please enter the value."
+            });
+        }
+
     }
     render() {
         return(
             <div>
-                <Titles/>
-                <Form getWeather={this.getWeather}/>
-                <Weather/>
+                <div className="wrapper">
+                    <div className="main">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-xs-5 title-container">
+                                    <Titles/>
+                                </div>
+                                <div className="col-xs-7 form-container">
+                                    <Form getWeather={this.getWeather}/>
+                                    <Weather
+                                        temperature={this.state.temperature}
+                                        city={this.state.city}
+                                        country={this.state.country}
+                                        humidity={this.state.humidity}
+                                        description= {this.state.description}
+                                        error= {this.state.error}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
